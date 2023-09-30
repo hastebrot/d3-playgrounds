16eebf36820c75ac97c9c1b1d8510de19cbb80c6
Benjamin Gudehus <benjamin.gudehus@freiheit.com>
2023-10-01 01:10:37 +0200

    feat: add icicle and sunburst functions.

--- sunburst-diagram/src/App.tsx
+++ sunburst-diagram/src/App.tsx
@@ -1,31 +1,31 @@
-import * as d3 from "d3";
 import { useEffect, useRef } from "react";
+import { createIcicle } from "./createIcicle";
+import { createSunburst } from "./createSunburst";
+import flareTree from "./flare-tree.json";
 
 export const App = () => {
-  const data = [{ label: "label" }];
-  const width = 200;
-  const height = 200;
-
-  const createSvg = () => {
-    const svg = d3
-      .create("svg")
-      .attr("viewBox", [0, 0, width, height])
-      .attr("width", width)
-      .attr("height", height)
-      .attr("style", "font: 12px sans-serif;");
-
-    const group = svg.selectAll("g").data(data).join("g");
-    group
-      .append("text")
-      .attr("y", 12)
-      .text((d) => d.label);
-
-    return svg.node();
-  };
-
   return (
-    <div style={{ padding: 10 }}>
-      <Chart svg={createSvg} />
+    <div style={{ display: "grid", padding: 10, gap: 10 }}>
+      <Chart
+        svg={() =>
+          createIcicle(flareTree, {
+            width: 600,
+            height: 600,
+            value: (d: any) => d.size,
+            label: (d: any) => d.name,
+          } as any)
+        }
+      />
+      <Chart
+        svg={() =>
+          createSunburst(flareTree, {
+            width: 600,
+            height: 600,
+            value: (d: any) => d.size,
+            label: (d: any) => d.name,
+          } as any)
+        }
+      />
     </div>
   );
 };
